2f2e8d3d9d15c7209bf67babee21be0818fb9328
TimurTaktarov <timurka.t.r@gmail.com>
2023-12-03 17:51:40 +0200

    first practice with docker

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ base_url = "http://api.open-notify.org/iss-now.json"
 while True:
     response = requests.get(url=base_url)
     data = response.json()
-    result = f'{datetime.datetime.now()};{data["iss_position"]} >>> {data["message"]}, timestamp: {data["timestamp"]}\n'
+    result = f'{datetime.datetime.now()};{data["iss_position"]} >>> {data["message"]}; timestamp: {data["timestamp"]}\n'
     print(result, end='')
 
     with open(f'statistic/{station}.csv', 'a') as file:
